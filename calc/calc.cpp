9e5ab568b6e5c12c0ecb8bb47b983f5927285a1b
202407008 <qjawns011019@naver.com>
2025-06-19 10:59:20 +0900

    calc.cpp add.cpp sub.cpp 코드 수정

--- calc/calc.cpp
+++ calc/calc.cpp
@@ -16,10 +16,16 @@ int main()
 
 	std::cout << "첫번째 수를 입력하세요 : ";
 	std::cin >> x;
+
+	if (-999 == x)
+	{
+		printf("프로그램을 종료합니다.\n");
+		break;
+	}
 	std::cout << "두번째 수를 입력하세요 : ";
 	std::cin >> y;
 
 	printf("입력 x[ %d ] y[ %d ]\n", x, y);
 	printf("add[ %d ] sub[ %d ] mul[ %d ] dive[ %d ] mod[ %d ]\n", 10, 6, 16, 4, 0);
-	//add(x, y), sub(x, y), mul(x, y), dive(x, y), mod(x, y));
+		//add(x, y), sub(x, y), mul(x, y), dive(x, y), mod(x, y));
 }
